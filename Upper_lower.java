971807609a5ba5c2bf3ca74991b5efd53d6e6b8e
Manjusri Arumugam <msri10792@gmail.com>
2024-11-05 20:15:37 +0530

    upper_lower

---
+++ Upper_lower.java
@@ -0,0 +1,9 @@
+package practise_problems;
+
+public class Upper_lower {
+	public static void main(String args[]) {
+		String str = "Java";
+		System.out.println("Uppercase: " + str.toUpperCase() + " Lowercase: " + str.toLowerCase());
+	}
+
+}
