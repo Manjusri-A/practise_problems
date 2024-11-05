a164b1bdc35908b08948b5aaaa501ba1ff476379
Manjusri Arumugam <msri10792@gmail.com>
2024-11-05 20:32:27 +0530

    wordcount

---
+++ Count_words.java
@@ -0,0 +1,12 @@
+package practise_problems;
+
+public class Count_words {
+
+	public static void main(String[] args) {
+		String str= "hello welcome to java";
+		String[] word = str.split(" ");
+		System.out.println(word.length);
+
+	}
+
+}
