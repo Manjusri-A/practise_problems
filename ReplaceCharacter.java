2a5d3c3512d43ca9e20dfe495fcdc385a6b36f75
Manjusri Arumugam <msri10792@gmail.com>
2024-11-11 19:11:17 +0530

    Add files via upload

---
+++ ReplaceCharacter.java
@@ -0,0 +1,13 @@
+package practise_problems;
+
+public class ReplaceCharacter {
+
+	public static void main(String[] args) {
+		String str = "hello world";
+		String replacedStr = str.replace('o', 'a'); 
+		System.out.println(replacedStr); 
+
+
+	}
+
+}
